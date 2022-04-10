1090f2492a02b74f715da1617421dc402330a2eb
Hidayah Rizki Ramadhan <58053633+hidayahrr@users.noreply.github.com>
2022-04-11 02:14:22 +0700

    Add files via upload

---
+++ code_converter_practice.py
@@ -0,0 +1,10 @@
+Korean_Civil_Airports_IATA_Code_Converter = {
+    "ICN": "Incheon International Airport",
+    "GMP": "Gimpo International Airport",
+    "CJU": "Jeju International Airport",
+    "MWX": "Muan International Airport",
+    "USN": "Ulsan Airport",
+    "YNY": "Yangyang International Airport",
+    "RSU": "Yeosu/Suncheon Airport",
+}
+print(Korean_Civil_Airports_IATA_Code_Converter[""])
